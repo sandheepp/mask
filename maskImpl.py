5d70ee1a4cbccd07969a148c0be3632f53a22423
Sandheep <broadsword@Anton.local>
2020-08-02 09:50:51 +0530

    added few modifications in the function

--- maskImpl.py
+++ maskImpl.py
@@ -26,7 +26,7 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
         Arrays of numpy arrays of values of whether mask is there or not
 
         Example:
-        
+
         {
                 'locs': [(239, 96, 310, 186), (80, 90, 164, 207)],
                 'preds': [[0.9927078 , 0.00729221],
@@ -41,6 +41,8 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
     # grab the dimensions of the frame and then construct a blob
     # from it
     (h, w) = frame.shape[:2]
+    # cv::dnn::blobFromImage (InputArray image, double scalefactor=1.0, const Size &size=Size(), const Scalar &mean=Scalar(), bool swapRB=false, bool crop=false, int ddepth=CV_32F)
+ 	# Creates 4-dimensional blob from image. Optionally resizes and crops image from center, subtract mean values, scales values by scalefactor, swap Blue and Red channels. More...
     blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
         (104.0, 177.0, 123.0))
 
@@ -94,6 +96,7 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
         # in the above `for` loop
         faces = np.array(faces, dtype="float32")
         preds = maskNet.predict(faces, batch_size=32)
+        
 
     # return a 2-tuple of the face locations and their corresponding
     # predictions
@@ -101,74 +104,74 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 
 
 if __name__ == "__main__":
-	# construct the argument parser and parse the arguments
-	ap = argparse.ArgumentParser()
-	ap.add_argument("-f", "--face", type=str,
-		default="face_detector",
-		help="path to face detector model directory")
-	ap.add_argument("-m", "--model", type=str,
-		default="mask_detector.model",
-		help="path to trained face mask detector model")
-	ap.add_argument("-c", "--confidence", type=float, default=0.5,
-		help="minimum probability to filter weak detections")
-	args = vars(ap.parse_args())
+    # construct the argument parser and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-f", "--face", type=str,
+        default="face_detector",
+        help="path to face detector model directory")
+    ap.add_argument("-m", "--model", type=str,
+        default="mask_detector.model",
+        help="path to trained face mask detector model")
+    ap.add_argument("-c", "--confidence", type=float, default=0.5,
+        help="minimum probability to filter weak detections")
+    args = vars(ap.parse_args())
 
-	# load our serialized face detector model from disk
-	print("[INFO] loading face detector model...")
-	prototxtPath = os.path.sep.join([args["face"], "deploy.prototxt"])
-	weightsPath = os.path.sep.join([args["face"],
-		"res10_300x300_ssd_iter_140000.caffemodel"])
-	faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+    # load our serialized face detector model from disk
+    print("[INFO] loading face detector model...")
+    prototxtPath = os.path.sep.join([args["face"], "deploy.prototxt"])
+    weightsPath = os.path.sep.join([args["face"],
+        "res10_300x300_ssd_iter_140000.caffemodel"])
+    faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
-	# load the face mask detector model from disk
-	print("[INFO] loading face mask detector model...")
-	maskNet = load_model(args["model"])
+    # load the face mask detector model from disk
+    print("[INFO] loading face mask detector model...")
+    maskNet = load_model(args["model"])
 
-	# initialize the video stream and allow the camera sensor to warm up
-	print("[INFO] starting video stream...")
-	vs = VideoStream(src=0).start()
-	time.sleep(2.0)
+    # initialize the video stream and allow the camera sensor to warm up
+    print("[INFO] starting video stream...")
+    vs = VideoStream(src=0).start()
+    time.sleep(2.0)
+    print("[INFO] Press 'q' to exit")
+    # loop over the frames from the video stream
+    while True:
+        # Grab the frame from the threaded video stream and resize it
+        # to have a maximum width of 400 pixels (width is always the greater value) 
+        frame = vs.read()
+        frame = imutils.resize(frame, width=400)
 
-	# loop over the frames from the video stream
-	while True:
-		# Grab the frame from the threaded video stream and resize it
-		# to have a maximum width of 400 pixels (width is always the greater value) 
-		frame = vs.read()
-		frame = imutils.resize(frame, width=400)
+        # detect faces in the frame and determine if they are wearing a
+        # face mask or not
+        (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
 
-		# detect faces in the frame and determine if they are wearing a
-		# face mask or not
-		(locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+        # loop over the detected face locations and their corresponding
+        # locations
+        for (box, pred) in zip(locs, preds):
+            # unpack the bounding box and predictions
+            (startX, startY, endX, endY) = box
+            (mask, withoutMask) = pred
 
-		# loop over the detected face locations and their corresponding
-		# locations
-		for (box, pred) in zip(locs, preds):
-			# unpack the bounding box and predictions
-			(startX, startY, endX, endY) = box
-			(mask, withoutMask) = pred
+            # determine the class label and color we'll use to draw
+            # the bounding box and text
+            label = "Mask" if mask > withoutMask else "No Mask"
+            color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
 
-			# determine the class label and color we'll use to draw
-			# the bounding box and text
-			label = "Mask" if mask > withoutMask else "No Mask"
-			color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+            # include the probability in the label
+            label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
-			# include the probability in the label
-			label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+            # display the label and bounding box rectangle on the output
+            # frame
+            cv2.putText(frame, label, (startX, startY - 10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+            cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
 
-			# display the label and bounding box rectangle on the output
-			# frame
-			cv2.putText(frame, label, (startX, startY - 10),
-				cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-			cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+        # show the output frame
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
 
-		# show the output frame
-		cv2.imshow("Frame", frame)
-		key = cv2.waitKey(1) & 0xFF
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
 
-		# if the `q` key was pressed, break from the loop
-		if key == ord("q"):
-			break
-
-	# do a bit of cleanup
-	cv2.destroyAllWindows()
-	vs.stop()
+    # do a bit of cleanup
+    cv2.destroyAllWindows()
+    vs.stop()
